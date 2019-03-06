4c525a75d80ed86db0ca8a7be0ebcd93e2ef2419
Paul Novikov <vokivon.p@gmail.com>
2019-03-06 23:21:41 +0300

    Реализовал сложение и умножение Task_02 решена

--- Lesson_05/Task_02.py
+++ Lesson_05/Task_02.py
@@ -8,6 +8,12 @@ import collections
 Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
 
+"""
+Решение:
+Функции суммы и произведения решил каждую своим способом, хотя можно было и через сложение сделать
+Также порезвился с разными коллекциями  ))) 
+"""
+
 HEXARR = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
           '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
 
@@ -15,8 +21,14 @@ HEXARR = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
 def dec_to_hex_one(d_num):
     return list(HEXARR.keys())[d_num]
 
+
 def hex_to_dec_all(hex_):
-    res = 0;
+    """
+    Переводит число из 16ричного числа в 10ричное
+    :param hex_: массив из символов шестнадцетиричного числа
+    :return: десятичный int
+    """
+    res = 0
     for item in hex_:
         res *= 16
         tmp_ = HEXARR[item]
@@ -24,31 +36,69 @@ def hex_to_dec_all(hex_):
     return res
 
 
+def dec_to_hex_all(num_):
+    """
+    Переводит число из 10ричного в 16ричное
+    :param num_: десятичный int
+    :return: массив из символов шестнадцетиричного числа
+    """
+    res = collections.deque()
+    tmp_next = num_
+    while tmp_next > 0:
+        tmp_h = tmp_next % 16
+        tmp_next //= 16
+        res.appendleft(dec_to_hex_one(tmp_h))
+    return list(res)
 
-def dec_plus_help(dec1, dec2, dec_mem = 0):
+
+def hex_to_str(hex_):
+    return ''.join(hex_)
+
+
+def str_to_hex(hex_str):
+    return [i for i in hex_str]
+
+
+def dec_hex_plus_help(dec1, dec2, dec_mem=0):
+    """
+    Складывает цифры 16_ричного числа в 10_ричном представлении
+    :param dec1: первая цифра
+    :param dec2: вторая цифра
+    :param dec_mem: цифра из памяти ( из предыдущих сложений )
+    :return: кортеж ( результат сложения цифр, цифра в память для следующего регистра )
+    """
     tmp_res = dec1 + dec2 + dec_mem
     res_mem = tmp_res // 16
     res_num = tmp_res % 16
     return res_num, res_mem
 
 
-def hex_plus(hex_1, hex_2):
+def hex_plus(h_1, h_2):
+    """
+    складывает 2 16х числа
+    :param h_1: 16х число в виде списка
+    :param h_2: 16х число в виде списка
+    :return: список содержащий результат сложения двух 16_ричных числа
+    """
     res = collections.deque()
     mem = 0
-    while len(hex_01) or len(hex_02):
-        if len(hex_01):
-            tmp_h1 = HEXARR[hex_01.pop()]
+    # копируем списки из аргументов, т.к дальше используем pop()
+    hex_1 = h_1.copy()
+    hex_2 = h_2.copy()
+
+    while len(hex_1) or len(hex_2):
+        if len(hex_1):
+            tmp_h1 = HEXARR[hex_1.pop()]
         else:
             tmp_h1 = 0
 
-        if len(hex_02):
-            tmp_h2 = HEXARR[hex_02.pop()]
+        if len(hex_2):
+            tmp_h2 = HEXARR[hex_2.pop()]
         else:
             tmp_h2 = 0
 
-        print(f"[{tmp_h1} + {tmp_h2} + {mem}]")
-
-        tmp_ = dec_plus_help(tmp_h1, tmp_h2, mem)
+        # print(f"[{tmp_h1} + {tmp_h2} + {mem}]")
+        tmp_ = dec_hex_plus_help(tmp_h1, tmp_h2, mem)
         num_ = dec_to_hex_one(tmp_[0])
         mem = tmp_[1]
         res.appendleft(num_)
@@ -56,31 +106,40 @@ def hex_plus(hex_1, hex_2):
         res.appendleft(dec_to_hex_one(mem))
     return list(res)
 
+
 def hex_mult(hex_1, hex_2):
-    return 0
+    """
+     Умножает два 16х числа
+    :param hex_1: первое 16х число
+    :param hex_2: второе 16х число
+    :return: произведение 16х чисел в виде списка
+    """
+    num_1 = hex_to_dec_all(hex_1)
+    num_2 = hex_to_dec_all(hex_2)
+    num_mult = num_1 * num_2
+    return dec_to_hex_all(num_mult)
 
 
-#hex_str1 = input("Введите первое HEX число: ")
-#hex_str2 = input("Введите второе HEX число: ")
-hex_str1 = "1F"
-hex_str2 = "FFF"
-print(hex_str1)
-print(hex_str2)
+hex_str1 = input("Введите первое HEX число: ")
+hex_str2 = input("Введите второе HEX число: ")
+# hex_str1 = "A2"
+# hex_str2 = "C4F"
+print()
+print(f"Первое число: {hex_str1}")
+print(f"Второе число: {hex_str2}")
+print()
 
-hex_01 = [i for i in hex_str1]
-hex_02 = [i for i in hex_str2]
+hex_01 = str_to_hex(hex_str1)
+hex_02 = str_to_hex(hex_str2)
+hex_sum = hex_plus(hex_01, hex_02)
+hex_mult = hex_mult(hex_01, hex_02)
+
 print(hex_01)
 print(hex_02)
 print()
-
-hex_sum = hex_plus(hex_01, hex_02)
 print(hex_sum)
-str_1 = ""
-print(f"Сумма цифр 0x{hex_str1} и 0x{hex_str2} = 0x{''.join(hex_sum)}")
-
-print(hex_to_dec_all([i for i in hex_str1]))
-
-
-
-#hex_02 = collections.deque()
-
+print(hex_mult)
+print()
+print(f"Сумма цифр 0x{hex_str1} и 0x{hex_str2} = 0x{hex_to_str(hex_sum)}")
+print()
+print(f"Произведение цифр 0x{hex_str1} и 0x{hex_str2} = 0x{hex_to_str(hex_mult)}")
