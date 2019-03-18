75137c8f7c0692ace6317c2f65e5eb4a0c584801
Paul Novikov <vokivon.p@gmail.com>
2019-03-18 17:46:08 +0300

    Начал делать задачу 2

---
+++ Lesson_08/Task_02.py
@@ -0,0 +1,13 @@
+__author__ = 'Павел Новиков (aka VokiVon)'
+
+"""
+2. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
+"""
+
+import collections
+
+collect = collections.Counter("мама мыла раму")
+print(collect)
+
+
+
